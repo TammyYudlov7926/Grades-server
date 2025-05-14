6e09b1b3a09bb0d865537146a4be39c85bd69b21
tammyyudlov <t0583287926@gmail.com>
2025-05-14 18:54:25 +0300

    Adding MODALS

--- Server_Project/Models/M_Grade.cs
+++ Server_Project/Models/M_Grade.cs
@@ -7,6 +7,39 @@ namespace Server_Project.Models
 {
     public class M_Grade
     {
-        
+        [BindRequired]
+        [Range(1, 100)]
+        public int ExeNumber { get; set; }//1 ,2 99= for the test
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public DateTime Date { get; set; }
+        public int GradeNumber { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        public string Comment { get; set; } = "defult comment";
+        public Grade Convert()
+        {
+            Grade g = new Grade()
+            {
+                ExeNumber = ExeNumber,
+                Name = Name,
+                Date = DateTime.Now,
+                Comment = Comment,
+                GradeNumber = GradeNumber
+            };
+            return g;
+
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
