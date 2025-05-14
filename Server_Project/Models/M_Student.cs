4242bd2bee5b51ff01f96cfee5c8591e92ef6434
tammyyudlov <t0583287926@gmail.com>
2025-05-14 18:45:33 +0300

    Getting started

--- Server_Project/Models/M_Student.cs
+++ Server_Project/Models/M_Student.cs
@@ -7,31 +7,5 @@ namespace Server_Project.Models
 {
     public class M_Student
     {
-        [BindRequired]
-        [StringLength(9)]
-        public string ID { get; set; }
-
-        [StringLength(50)]
-        public string Name { get; set; }
-
-        [StringLength(9)]
-        public string Password { get; set; }
-
-        public List<Grade> ExeList { get; set; } = new List<Grade>(); // אתחול ברירת מחדל
-
-        public Grade TestGrade { get; set; }
-
-        public Student Convert()
-        {
-            Student student = new Student()
-            {
-                ID = ID,
-                Name = Name,
-                Password = Password,
-                ExeList = ExeList ?? new List<Grade>(), // בדוק אם ExeList לא null
-                TestGrade = TestGrade ?? new Grade()
-            };
-            return student;
-        }
     }
 }
--- Server_Project/Services/AddGradesRequest.cs
+++ Server_Project/Services/AddGradesRequest.cs
@@ -4,8 +4,6 @@ namespace Server_Project.Services
 {
     public class AddGradesRequest
     {
-        public List<M_Grade> M_Grade { get; set; }
-        public List<string> ListId { get; set; }
-        public int ExeNumber { get; set; }
+       
     }
 }
