46fbde5fe2ca86eecbb82595dc9606f2e8011a39
tammyyudlov <t0583287926@gmail.com>
2025-05-14 18:59:13 +0300

    Add SERVICES

--- Server_Project/Services/AddGradesRequest.cs
+++ Server_Project/Services/AddGradesRequest.cs
@@ -4,6 +4,8 @@ namespace Server_Project.Services
 {
     public class AddGradesRequest
     {
-       
+        public List<M_Grade> M_Grade { get; set; }
+        public List<string> ListId { get; set; }
+        public int ExeNumber { get; set; }
     }
 }
